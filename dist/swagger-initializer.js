8715dadaf48d90e24bdac9ccea9e1028e7e7e598
Sergey S <79587538+sashapar23@users.noreply.github.com>
2024-10-18 22:52:02 +0300

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "aws-openai-classifier-api-v0.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
